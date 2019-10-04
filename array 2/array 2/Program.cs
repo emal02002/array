5b17f62091eb467c95caf240ed8e840169ea62a2
Emad <emal02002@utb.vaxjo.se>
2019-10-04 12:23:19 +0200

    har gjort klart array 2

---
+++ array 2/array 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace array_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperatur = { 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9 };
+            Console.WriteLine("Medeltemperatur:" + Medel(temperatur));
+        }
+         public static double Medel(double[] temperatur)
+        {
+            double summa = 0;
+            for (int i = 0; i < temperatur.Length; i++)
+                summa += temperatur[i];
+            return (summa / temperatur.Length);
+
+        }
+    }
+}
