f5b7114dea5582154b12b411ef51036a0a14ecc7
emal02002 <54432912+emal02002@users.noreply.github.com>
2019-10-04 23:42:18 +0200

    Update Program.cs

--- array 2/array 2/Program.cs
+++ array 2/array 2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace array_2
 {
@@ -7,7 +7,8 @@ namespace array_2
         static void Main(string[] args)
         {
             double[] temperatur = { 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9 };
-            Console.WriteLine("Medeltemperatur:" + Medel(temperatur));
+            Console.WriteLine("Medeltemperatur:" + Math.Round(Medel(temperatur),2));
+           
         }
          public static double Medel(double[] temperatur)
         {
@@ -15,6 +16,7 @@ namespace array_2
             for (int i = 0; i < temperatur.Length; i++)
                 summa += temperatur[i];
             return (summa / temperatur.Length);
+            
 
         }
     }
