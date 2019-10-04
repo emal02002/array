adf4f017656d70555e2125fee8c1cf32460cc169
Emad <emal02002@utb.vaxjo.se>
2019-10-04 08:38:31 +0200

    första biten är klar

---
+++ Array 1/Array 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[5];
+            for (int i = 0; i < tal.Length; i++)
+            {
+                Console.WriteLine("skriv tal");
+                Console.ReadLine();
+            }
+
+            
+
+
+
+
+        }
+    }
+}
